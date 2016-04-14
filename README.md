9f18985f8a2c35b1265ee5e488b84b317fc4c2bf
Jane Kim <jane@janekim.me>
2016-04-14 14:07:18 -0400

    update to jquery and instructions

---
+++ README.md
@@ -0,0 +1,25 @@
+# Madlibs
+
+1. Fork this repository and create a new workspace in cloud9
+2. Create a story with ten parts of speech
+
+In the following files...
+
+### `index.html` 
+* new input for each word you want to ask for with an ID for each one 
+```html
+<input type="text" class="form-control input-lg" id="newIDHere" placeholder="your prompt here">
+```
+
+### `app.js`
+* Add new words you want the user to fill in to the variable `story`
+* for each input you have
+```js
+inputValue = $('#newIDHere').val();
+story = story.replace("PLURAL_NOUN", inputValue);
+```
+* Two of the parts of speech are already there for you. You will need to add 8 more. Get creative!
+* bonus: bold the words that the user added
+
+### bonus: style.css
+Style your madlibs! 
--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 function generateMadlib() {
   
   // This is the starting story
-  var story = "Welcome to ScriptEd! Our mascot is the PLURAL_NOUN.  We love to VERB! ";
+  var story = 'Welcome to ScriptEd! Our mascot is the PLURAL_NOUN. We love to VERB!';
 
-  var inputValue = document.getElementById("plural-noun-1").value;
+  var inputValue = $('#plural-noun-1').val();
   story = story.replace("PLURAL_NOUN", inputValue);
   
-  inputValue = document.getElementById("verb").value;
+  inputValue = $('#verb').val();
   story = story.replace("VERB", inputValue);
   
   // This writes the story when the user clicks the "Generate MadLib" button"
-  document.getElementById("result").innerHTML = story;
+  $('#result').text(story);
 }
+
+$(document).ready(function() {
+	$('.btn').click(function() {
+		generateMadLib();	
+	})
+});
